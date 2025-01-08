fac751d7f3c2dc815733bffdbae0271c60c41457
Sam Schwartz <sam@cs.uoregon.edu>
2025-01-08 15:13:30 -0800

    File structure refactor

--- src/moss/lib/models/base.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-from neomodel import StructuredNode, StringProperty, RelationshipTo
-
-class Category(StructuredNode):
-    name = StringProperty(required=True)
-    description = StringProperty(required=True)
--- src/moss/lib/models/relationships.py
+++
@@ -1,7 +0,0 @@
-from neomodel import StructuredRel, DateTimeProperty, FloatProperty
-
-class RelatedTo(StructuredRel):
-    score = FloatProperty()
-
-class ContributedTo(StructuredRel):
-    date = DateTimeProperty()
